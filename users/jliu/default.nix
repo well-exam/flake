9c9b30cee6b0c3493fda719c2601bd9653ea612f
Jianbo Liu <jliu32@nd.edu>
2023-05-01 04:16:23 -0400

    fix bootstrap

--- users/jliu/default.nix
+++ users/jliu/default.nix
@@ -8,7 +8,7 @@
       home = "/home/jliu";
       createHome = true;
       passwordFile = "/persist/encrypted-passwords/jliu";
-      progams.fish.enable = true;
+      programs.fish.enable = true;
       shell = pkgs.fish;
       extraGroups = [ "wheel" "disk" "networkmanager" "libvirtd" "qemu-libvirtd" "kvm" "i2c" "plugdev" ];
       openssh.authorizedKeys.keys = [
