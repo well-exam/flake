6813bf0e4d06a7ddb994d535a630b3556d9115d4
Jianbo Liu <public@jliu.net>
2023-05-01 00:02:39 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,150 @@
+{
+  description = "Jianbo's Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
+    {
+      overlays.default = final: prev: {
+        neovimConfigured = final.callPackage ./packages/neovimConfigured { };
+      };
+
+      packages = forAllSystems
+        (system:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ self.overlays.default ];
+              config.allowUnfree = true;
+            };
+          in
+          {
+            inherit (pkgs) neovimConfigured;
+
+            unsafe-bootstrap = pkgs.callPackage ./packages/unsafe-bootstrap { };
+          });
+
+      devShells = forAllSystems
+        (system:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ self.overlay.default ];
+            };
+          in
+          {
+            default = pkgs.mkShell
+              {
+                inputsFrom = with pkgs; [ ];
+                buildInputs = with pkgs; [
+                  nixpkgs-fmt
+                ];
+              };
+          });
+
+      homeConfigurations = forAllSystems
+        (system:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ self.overlay.default ];
+            };
+          in
+          {
+            jliu = home-manager.lib.homeManagerConfiguration {
+              inherit pkgs;
+              modules = [
+                ./users/jliu/home.nix
+              ];
+            };
+          }
+        );
+
+      nixosConfigurations =
+        let
+          # aarch64Base = {
+          #   system = "aarch64-linux";
+          #   modules = with self.nixosModules; [
+          #     ({ config = { nix.registry.nixpkgs.flake = nixpkgs; }; })
+          #     home-manager.nixosModules.home-manager
+          #     traits.overlay
+          #     traits.base
+          #     services.openssh
+          #   ];
+          # };
+          x86_64Base = {
+            system = "x86_64-linux";
+            modules = with self.nixosModules; [
+              ({ config = { nix.registry.nixpkgs.flake = nixpkgs; }; })
+              home-manager.nixosModules.home-manager
+              traits.overlay
+              traits.base
+              services.openssh
+            ];
+          };
+        in
+        with self.nixosModules; {
+          x86_64IsoImage = nixpkgs.lib.nixosSystem {
+            inherit (x86_64Base) system;
+            modules = x86_64Base.modules ++ [
+              platforms.iso
+            ];
+          };
+          carbon = nixpkgs.lib.nixosSystem {
+            inherit (x86_64Base) system;
+            modules = x86_64Base.modules ++ [
+              platforms.carbon
+              traits.machine
+              traits.workstation
+              traits.gnome
+              traits.hardened
+              users.jliu
+            ];
+          };
+        };
+
+      nixosModules = {
+        platforms.carbon = ./platforms/carbon.nix;
+        platforms.ios = "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-gnome.nix";
+        traits.overlay = { nixpkgs.overlays = [ self.overlays.default ]; };
+        traits.base = ./traits/base.nix;
+        traits.machine = ./traits/machine.nix;
+        traits.gaming = ./traits/gamin.nix;
+        traits.gnome = ./traits/gnome.nix;
+        traits.jetbrains = ./traits/jetbrains.nix;
+        traits.hardened = ./traits/hardened.nix;
+        traits.sourceBuild = ./traits/source-build.nix;
+        services.postgres = ./services/postgres.nix;
+        services.openssh = ./services/openssh.nix;
+        traits.workstation = ./traits/workstation.nix;
+        user.jliu = ./user/jliu;
+      };
+
+      checks = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ self.overlays.default ];
+          };
+        in
+        {
+          format = pkgs.runCommand "check-format"
+            {
+              buildInputs = with pkgs; [ rustfmt cargo ];
+            } ''
+            ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt --check ${./.}
+            touch $out # it worked!
+          '';
+        });
+    };
+}
